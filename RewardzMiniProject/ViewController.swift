10bce3927bee97472cdf81c5fdedd90ef706a4cb
Christopher Sonny <140031525+christophersonny@users.noreply.github.com>
2023-08-30 18:08:32 +0700

    initial commit for rewardz mini project

---
+++ RewardzMiniProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RewardzMiniProject
+//
+//  Created by Christopher Sonny on 30/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
